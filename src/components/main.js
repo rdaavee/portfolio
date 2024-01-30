9fc9f745a723864993ff9051bf225d869e73c09e
ran <rdavee0412@gmail.com>
2024-01-30 22:24:46 +0800

    added scrollreveal animation for cards

---
+++ src/components/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+    ScrollReveal().reveal('.card', {
+        delay: 200,
+        distance: '20px',
+        easing: 'ease-in-out',
+        origin: 'bottom',
+        reset: true,
+        interval: 100
+    });
+});
